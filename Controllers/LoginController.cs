2839b2c88a81da4fc04e243f232fcd9f40f4d7f0
marcod08 <marcodimpirma91@gmai.com>
2024-03-04 16:54:20 +0100

    first commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using S6L1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace S6L1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Author author) 
+        {
+            var hasValidCredentials = false;
+
+            // qui inserire la logica per controllare username e pass nel database
+            hasValidCredentials = true;
+
+            if (hasValidCredentials)
+            {
+                FormsAuthentication.SetAuthCookie(author.AuthorId.ToString(), true);
+                return RedirectToAction("", ""); // collegamento alla pagina del pannello
+            }
+            return RedirectToAction("Index");
+
+        }
+        [Authorize]
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
